25df4fb20efcfae594fd0858e346d22fc2ffe0b0
Kaur Jaanson <kjaanson@gmail.com>
2019-11-08 22:53:57 +0200

    Added updated maroon conf

--- jupyterhub-maroon/roles/jupyterhub/files/jupyterhub_config.py
+++ jupyterhub-maroon/roles/jupyterhub/files/jupyterhub_config.py
@@ -1,11 +1,7 @@
 import os
-from jupyter_client.localinterfaces import public_ips
 
-ip = public_ips()[0]
+c.Application.log_level = 'INFO'
 
-c.Application.log_level = 'DEBUG'
-
-c.JupyterHub.hub_ip = ip
 c.JupyterHub.ip = '0.0.0.0'
 c.JupyterHub.port = 443
 
@@ -16,16 +12,11 @@ c.JupyterHub.hub_port = 8181
 c.JupyterHub.ssl_key = '/opt/jupyterhub/ssl/key.pem'
 c.JupyterHub.ssl_cert = '/opt/jupyterhub/ssl/cert.pem'
 
-
-c.JupyterHub.authenticator_class = 'dummyauthenticator.DummyAuthenticator'
-
-c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
-
-c.DockerSpawner.image = 'bioinf-jupyterhub-singleuser'
-
-# This should limit the env variables from jupyterhub to jupter notebook. 
+# This should limit the env variables from jupyterhub to jupter notebook.
 # Not sure if it works when notebook is started by sudospawner.
 #c.Spawner.env_keep = ['PATH',
 #                      'PYTHONPATH']
+
+c.Spawner.default_url = '/lab'
 
 c.Authenticator.admin_users = {'kaur'}
